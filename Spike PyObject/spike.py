068a222131c6e4fad085cc444e928a3bb046a4c5
OKOLIMO-Joseph <137505067+OKOLIMO-Joseph@users.noreply.github.com>
2024-09-16 20:49:29 +0300

    Objects Nike spikes

---
+++ Spike PyObject/spike.py
@@ -0,0 +1,13 @@
+class Spike:
+
+    def __init__ (self, make, model, color, size):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.size = size
+
+    def grip(self):
+        print('It has held the blocks tight')
+
+    def fly(self):
+        print('It is flying high')
