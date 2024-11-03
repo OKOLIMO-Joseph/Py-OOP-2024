e0b75ad728cd33617a6c734ca015bd8c24b375dd
Joseph_OKOLIMO <137505067+OKOLIMO-Joseph@users.noreply.github.com>
2024-11-03 18:59:49 +0300

    Spikes object 1

---
+++ spikes.py
@@ -0,0 +1,11 @@
+from spike import Spike
+
+spike_1 = Spike('Nike','ZoomX','Organe','Medium')
+spike_2 = Spike('Adidas','Adizeiro','Green','Large')
+
+print(spike_2.make)
+print(spike_2.model)
+print(spike_2.color)
+print(spike_2.size)
+
+spike_2.grip()
