a04599385e9e72912d0ddeb10edd75acf93e4a8c
Joseph_OKOLIMO <137505067+OKOLIMO-Joseph@users.noreply.github.com>
2024-11-28 22:01:36 +0300

    Class methods in Python OOP

---
+++ POOP/clsmth.py
@@ -0,0 +1,10 @@
+class Football:
+    team = 'Arsenal'
+    def __init__(self, epl_position):
+        self.epl_position = epl_position 
+    #Decalring the class method
+    @classmethod
+    def my_team(cls):
+        return cls.team
+# Passing on the class method onto the classs itself
+print(Football.my_team()) 
